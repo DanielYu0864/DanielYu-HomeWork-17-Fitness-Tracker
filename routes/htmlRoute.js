d98ee461869803cc1ab44b271b615c18c259a7e2
Daniel.Yu <adam741963@gmail.com>
2020-10-16 16:06:51 -0700

    html route file

--- routes/htmlRoute.js
+++ routes/htmlRoute.js
@@ -1,15 +1,16 @@
-const db = require('../models');
+// path to point the directory to the file
 const path = require('path');
 
 module.exports = (app) => {
-  // app.get('/', (req, res) => {
-  //   res.sendFile(path.join(__dirname, 'public/index.html'));
-  // });
-
+  // router for the index.html
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+  });
+  // router for the  exercise page
   app.get('/exercise', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/exercise.html'));
   });
-
+  // router for the stats page
   app.get('/stats', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/stats.html'));
   });
