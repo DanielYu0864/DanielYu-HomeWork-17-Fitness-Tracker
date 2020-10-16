13e809218f38a2dfa0233f6945f1b25c845c6d2f
Daniel.Yu <adam741963@gmail.com>
2020-10-16 16:22:50 -0700

    api route comment

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,6 +1,7 @@
 const db = require('../models');
 
 module.exports = (app) => {
+  // get route for getLastWorkout() function to get the last document
   app.get('/api/workouts', async (req, res) => {
     await db.Workout.find({}).then(workouts => {
       res.json(workouts);
@@ -10,6 +11,7 @@ module.exports = (app) => {
       res.status(400).json(err);
     });
   });
+  // get route for getWorkoutsInRange() function to get the work out detail for the dashboard page
   app.get('/api/workouts/range', async (req, res) => {
     await db.Workout.find({}).then(workouts => {
       res.json(workouts);
@@ -19,6 +21,7 @@ module.exports = (app) => {
       res.status(400).json(err);
     });
   });
+  // post route to create a new document with id, day info, and empty exercises array
   app.post('/api/workouts', async (req, res) => {
     await db.Workout.create({})
       .then(workoutdb => {
@@ -29,8 +32,9 @@ module.exports = (app) => {
         res.status(400).json(err);
       });
   });
+  // put route for addExerise() function to push new exercises object to the current document
   app.put('/api/workouts/:id', async ({body, params}, res) => {
-    console.log(body, params.id);
+    // console.log(body, params.id);
     await db.Workout.findByIdAndUpdate(params.id, { $push: {exercises: body} })
       .then((workout) => {
         console.log('added', workout)
